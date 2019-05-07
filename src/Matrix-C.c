3a9bf671b97bc14ef9c96f93b990b6a020d9cb8c
mamba2410 <callummcgregor54@gmail.com>
2019-05-07 12:18:20 +0100

    Added storing and loading matrices in binary files

--- src/Matrix-C.c
+++ src/Matrix-C.c
@@ -7,26 +7,14 @@
 int main(int argc, char** argv){
 	srand(time(NULL));
 	
-	matrix_t m4 = m_make(4, 4);
-	matrix_t m1 = m_make(3, 2);
-	m_noise(m4);
-	m_noise(m1);
-	
-	m_storef(m4, "m4.m", 'w');
-	m_storef(m1, "m4.m", 'a');
-	
 	matrix_t *ms = m_bloadf("m4.m", 2);
 	
-	m_printf(m4);
 	m_printf(ms[0]);
-
-	m_printf(m1);
 	m_printf(ms[1]);
 
-	m_destroy(m1);
 	m_destroy(ms[1]);
-	m_destroy(m4);
 	m_destroy(ms[0]);
+	free(ms);
 	return 0;
 }
 
